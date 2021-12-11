1f6a4c33377a6d2830610993b4612191030e7cf4
Arnav Shah <arnavshah@Arnavs-Laptop.hsd1.wa.comcast.net>
2021-12-10 17:23:39 -0800

    test

--- src/test/Contract.t.sol
+++ src/test/Contract.t.sol
@@ -1,12 +1,31 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.10;
 
-import "ds-test/test.sol";
+import "../../lib/ds-test/src/test.sol";
+import "../gen2.sol";
+import "../main.sol";
+import "../token.sol";
 
 contract ContractTest is DSTest {
-    function setUp() public {}
+    Main main;
+    YieldToken yield;
+    Gen2 gen2;
 
-    function testExample() public {
-        assertTrue(true);
+    function setUp() public {
+        bytes32 root = 0x4d32c58ef0a2d0cea440002d346d7d413c49f93ec252522642e1e4937e85089a;
+        address treasury = 0x6cB5FceFa0037c373aD1Ec7717874117D353bF49;
+        address planar = 0x495f947276749Ce646f68AC8c248420045cb7b5e;
+        main = new Main(root, treasury, planar);
+        yield = new YieldToken(address(main));
+        gen2 = new Gen2(address(main));
+    } 
+
+    function testsetTokenAddyAndCouncilWL() public {
+        main.setTokenAddresses(address(yield), address(gen2));
+        main.changeStatus(1);
+    }
+
+    function testSuperMint() public {
+        main.superMint();
     }
 }
